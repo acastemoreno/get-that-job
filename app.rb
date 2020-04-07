f495c8c04da8c0d22f485317ac4a51f537a2b3c0
Albert <acastemoreno@gmail.com>
2020-04-07 09:05:28 -0500

    cache control all pages

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 require "sinatra"
 
 get "/:page?" do
+  cache_control :public, max_age: 1
   if params["page"].nil?
-    cache_control :public, max_age: 1
     erb :index, layout: :layout
   else
     erb params["page"].to_sym, layout: :layout
